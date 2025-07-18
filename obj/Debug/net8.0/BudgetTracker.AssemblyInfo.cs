ba901ae5ab726b2e80bca65162480085c449b9f7
ToniPavlovic <tonipavlovic518@gmail.com>
2025-07-18 22:49:17 +0200

    initial commit

--- obj/Debug/net8.0/BudgetTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/BudgetTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d299dd3fbd0a8e79ff164488a8197aefa1caf856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab257eaecc7cdbaef4ba142690e6db13331f693")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
